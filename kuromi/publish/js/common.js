fd000389b0405ed92a4d54f6f7a1ff74884df9eb
Rumyyyyy <87421765+Rumyyyyy@users.noreply.github.com>
2021-09-15 21:04:51 +0900

    .

---
+++ kuromi/publish/js/common.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('div header nav > ul > li').hover(function(){
+        $('div header > nav').addClass('active');
+    },function(){
+        $('div header > nav').removeClass('active')
+    });
+});
